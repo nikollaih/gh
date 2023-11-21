e83e7ef39109531e6f398610ec2c940ee9011f13
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2023-11-21 18:27:04 -0500

    Validacion login

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
 
 	public function dashboard(){
+		is_logged(false, false);
 		$this->load->view('dashboard/index');
 	}
 }
