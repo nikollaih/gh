9b5eca6b992e9eadbac39a3755804cc39dd244f7
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2023-11-22 22:09:09 -0500

    Session

--- application/config/config.php
+++ application/config/config.php
@@ -388,7 +388,7 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'gh_session';
-$config['sess_expiration'] = 7200;
+$config['sess_expiration'] = 604800;
 $config['sess_save_path'] = "ci_sessions";
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
